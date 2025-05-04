60b5973791635a900b28cf449d30603cd4802967
Kalai Shelton <kalaishelton@Kalais-MacBook-Pro.local>
2025-05-04 18:44:27 +0530

    first commit

---
+++ src/Components/Counter.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+
+const Counter = () => {
+    const [count, setCount]=useState(0)
+    const handleClick = ()=>{
+        setCount(count+1)
+    }
+  return (
+    <div className='flex flex-col justify-center h-40vh'>
+        {count}
+        <div>
+       <button className='border-4 rounded-md border-red-400' onClick={handleClick}>Increase</button> 
+       </div>
+    </div>
+  )
+}
+
+export default Counter
